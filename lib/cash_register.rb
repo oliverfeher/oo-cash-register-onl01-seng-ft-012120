08f09e7d5f6c70d9c6d70fa64fc804a014555b62
Oliver Feher <olcsee@gmail.com>
2020-02-02 01:01:12 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -41,11 +41,9 @@ class CashRegister
   end
   
   def void_last_transaction
-    if @items = []
-      return 0.0
-    end
+    
     @items.pop
-    @total= @total - @price
+    @total.to_f = @total - @price
   end
   
 end
